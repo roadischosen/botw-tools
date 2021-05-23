c50263e8c555979617d7419086be32bb8c33fb0e
roadischosen <awesomenamesmail@gmail.com>
2021-05-23 14:34:12 +0300

    extract link graph given one hash; include actor names

--- convert.py
+++ convert.py
@@ -2,45 +2,78 @@ import os
 import sys
 import json
 import xml.etree.ElementTree as ET
+import struct
 
-if len(sys.argv) < 2:
-  print(f"Usage {sys.argv[0]} MUBIN-DIR")
-  exit()
-else:
-  mubin_dir = sys.argv[1]
+def prepare_json(indir, outfile):
+  '''[
+    {
+      "id":    <hashid>,
+      "links": [
+        {
+          "id":   <hashid>,
+          "type": <type>
+        },
+        ...
+      ]
+    },
+    ...
+  ]'''
+  objects = []
+  for filename in os.listdir(indir):
+    # no obvious way to list paths
+    root = ET.parse(os.path.join(indir, filename)).getroot()
 
-'''[
-  {
-    "id":    <hashid>,
-    "links": [
-      {
-        "id":   <hashid>,
-        "type": <type>
-      },
-      ...
-    ]
-  },
-  ...
-]'''
-objects = []
-for filename in os.listdir(mubin_dir):
-  # no obvious way to list paths
-  root = ET.parse(os.path.join(mubin_dir, filename)).getroot()
+    for obj in root.findall("./Objs/value"):
+      hashid = obj.get("HashId")
+      if hashid:
+        name = obj.find("UnitConfigName")
+        name = name.text if (name is not None and name.text) else "Unknown"
+        entry = {"id": hashid, "name": name, "links": []}
+        for link in obj.findall("./LinksToObj/value"):
+          hashid = link.get("DestUnitHashId")
+          if hashid:
+            ltype = link.find("DefinitionName")
+            entry["links"].append({
+              "id": hashid,
+              # bool(ltype) is False, because the Element has no children
+              "type": ltype.text if (ltype is not None and ltype.text) else "Unknown"})
+        objects.append(entry)
+    print(filename)
 
-  for obj in root.findall("./Objs/value"):
-    hashid = obj.get("HashId")
-    if hashid:
-      entry = {"id": hashid, "links": []}
-      for link in obj.findall("./LinksToObj/value"):
-        hashid = link.get("DestUnitHashId")
-        if hashid:
-          ltype = link.find("DefinitionName")
-          entry["links"].append({
-            "id": hashid,
-            # bool(ltype) is False, because the Element has no children
-            "type": ltype.text if (ltype is not None and ltype.text) else "Unknown"})
-      objects.append(entry)
-  print(filename)
+  with open(outfile, "w") as out:
+    json.dump(objects, out, indent=2)
 
-with open(mubin_dir+".json", "w") as out:
-  json.dump(objects, out, indent=2)
+def parse_hashid(s):
+  if s.startswith("0x"):
+    return str(struct.unpack(">i", bytes.fromhex(s[2:]))[0])
+  else:
+    assert -(2**32) <= int(s) <= (2**32-1)
+    return s
+
+def main():
+  if len(sys.argv) < 3:
+    print(f"Usage {sys.argv[0]} MUBIN-DIR HASHID")
+    exit()
+
+  mubin_dir  = sys.argv[1]
+  mubin_json = mubin_dir+".json"
+  hashids = [parse_hashid(sys.argv[2])]
+
+  if not os.path.isfile(mubin_json):
+    prepare_json(mubin_dir, mubin_json)
+
+  with open(mubin_json, "r") as inf:
+    objects = json.load(inf)
+
+  for hashid in hashids:
+    for obj in objects:
+      s = str(obj)
+      if s.find(hashid) != -1:
+        print(s)
+        for link in obj["links"]:
+          hid = link["id"]
+          if hid not in hashids:
+            hashids.append(link["id"])
+
+if __name__ == '__main__':
+  main()
